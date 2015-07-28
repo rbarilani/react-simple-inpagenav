d9459fbba68e5566c8fa7e39e91a6125e0a58556
Ruben Barilani <ruben.barilani@zalando.de>
2015-07-28 18:03:58 +0200

    build

--- dist/react-simple-inpagenav.js
+++ dist/react-simple-inpagenav.js
@@ -3,56 +3,7 @@
 'use strict';
 
 var React = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null);
-var $ = (typeof window !== "undefined" ? window['$'] : typeof global !== "undefined" ? global['$'] : null);
-var _ = (typeof window !== "undefined" ? window['_'] : typeof global !== "undefined" ? global['_'] : null);
-var $scrollTo = (typeof window !== "undefined" ? window['$']['scrollTo'] : typeof global !== "undefined" ? global['$']['scrollTo'] : null);
-
-var CONSTANTS = {
-    SECTION_ID_SUFFIX: '__simple-inpagenav',
-    DEBOUNCED_SCROLL_EVENT: 'simple-inpagenav.scroll',
-    SCROLL_EVENT: 'scroll.simpleinapgenav',
-    LOAD_EVENT: 'load.simpleinpagenav',
-    RESIZE_EVENT: 'resize.simpleinpagenav',
-    DEBOUNCED_SCROLL_EVENT_DELAY: 300,
-    DEFAULT_OPTIONS: {
-        scrollThreshold: 0.4,
-        scrollOffset: 50,
-        scrollTo: {
-            duration: 300,
-            offset: -40
-        }
-    }
-};
-
-/**
- *
- * BarItem Component
- * @type {BarItem}
- *
- */
-var BarItem = React.createClass({
-    displayName: 'BarItem',
-
-    propTypes: {
-        target: React.PropTypes.string.isRequired
-    },
-    render: function render() {
-        var className = this.props.currentTarget === this.props.target ? 'active' : '';
-        return React.createElement(
-            'li',
-            null,
-            React.createElement(
-                'a',
-                { href: '#' + this.props.target, className: className, onClick: this.onClick },
-                this.props.children
-            )
-        );
-    },
-    onClick: function onClick(event) {
-        event.preventDefault();
-        this.props.onClickCallback(this.props.target); // inform parent that a navbar item was clicked!
-    }
-});
+var BarItem = require('./BarItem');
 
 /**
  *
@@ -105,6 +56,77 @@ var Bar = React.createClass({
     }
 });
 
+module.exports = Bar;
+
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{"./BarItem":2}],2:[function(require,module,exports){
+(function (global){
+'use strict';
+
+var React = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null);
+
+/**
+ *
+ * BarItem Component
+ * @type {BarItem}
+ *
+ */
+var BarItem = React.createClass({
+    displayName: 'BarItem',
+
+    propTypes: {
+        target: React.PropTypes.string.isRequired
+    },
+    render: function render() {
+        var className = this.props.currentTarget === this.props.target ? 'active' : '';
+        return React.createElement(
+            'li',
+            null,
+            React.createElement(
+                'a',
+                { href: '#' + this.props.target, className: className, onClick: this.onClick },
+                this.props.children
+            )
+        );
+    },
+    onClick: function onClick(event) {
+        event.preventDefault();
+        this.props.onClickCallback(this.props.target); // inform parent that a navbar item was clicked!
+    }
+});
+
+module.exports = BarItem;
+
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{}],3:[function(require,module,exports){
+'use strict';
+
+var CONSTANTS = {
+    SECTION_ID_SUFFIX: '__simple-inpagenav',
+    DEBOUNCED_SCROLL_EVENT: 'simple-inpagenav.scroll',
+    SCROLL_EVENT: 'scroll.simpleinapgenav',
+    LOAD_EVENT: 'load.simpleinpagenav',
+    RESIZE_EVENT: 'resize.simpleinpagenav',
+    DEBOUNCED_SCROLL_EVENT_DELAY: 300,
+    DEFAULT_OPTIONS: {
+        scrollThreshold: 0.4,
+        scrollOffset: 50,
+        scrollTo: {
+            duration: 300,
+            offset: -40
+        }
+    }
+};
+
+module.exports = CONSTANTS;
+
+},{}],4:[function(require,module,exports){
+(function (global){
+'use strict';
+
+var React = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null);
+var CONSTANTS = require('./Constants');
+
 /**
  *
  * Section Component
@@ -131,6 +153,23 @@ var Section = React.createClass({
         );
     }
 });
+
+module.exports = Section;
+
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{"./Constants":3}],5:[function(require,module,exports){
+(function (global){
+'use strict';
+
+var React = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null);
+var $ = (typeof window !== "undefined" ? window['$'] : typeof global !== "undefined" ? global['$'] : null);
+var _ = (typeof window !== "undefined" ? window['_'] : typeof global !== "undefined" ? global['_'] : null);
+var $scrollTo = (typeof window !== "undefined" ? window['$']['scrollTo'] : typeof global !== "undefined" ? global['$']['scrollTo'] : null);
+
+var CONSTANTS = require('./Constants');
+var BarItem = require('./BarItem');
+var Bar = require('./Bar');
+var Section = require('./Section');
 
 /**
  *
@@ -189,18 +228,20 @@ var SimpleInpagenav = React.createClass({
     render: function render() {
         var sectionChildrenFound = false;
         var children = React.Children.map(this.props.children, function (child) {
-            if (child.type === Section) {
-                sectionChildrenFound = true;
-                return React.cloneElement(child, {
-                    registerSection: this.registerSection
-                });
-            } else if (child.type === Bar) {
-                return React.cloneElement(child, {
-                    currentTarget: this.state.currentTarget,
-                    onItemClickCallback: this.onItemClick
-                });
+            switch (child.type) {
+                case Section:
+                    sectionChildrenFound = true;
+                    return React.cloneElement(child, {
+                        registerSection: this.registerSection
+                    });
+                case Bar:
+                    return React.cloneElement(child, {
+                        currentTarget: this.state.currentTarget,
+                        onItemClickCallback: this.onItemClick
+                    });
+                default:
+                    return child;
             }
-            return child;
         }, this);
         // we need at least a section child
         if (!sectionChildrenFound) {
@@ -212,25 +253,57 @@ var SimpleInpagenav = React.createClass({
             children
         );
     },
+    /**
+     * Flag to react or don't react when user scroll (example: when we scroll the window with scrollTo)
+     * @type {boolean}
+     */
     shouldReactOnScroll: true,
+    /**
+     * Save a jquery window reference for further use
+     * @type {jQuery}
+     */
     $window: $(window),
+    /**
+     * Map like object for registered sections
+     * @type {object}
+     */
     sections: {},
+    /**
+     * @type {object}
+     */
     options: _.merge({}, CONSTANTS.DEFAULT_OPTIONS),
+    /**
+     * Updates currentTarget state property
+     * @param {string} [target]
+     */
     updatesCurrentTarget: function updatesCurrentTarget(target) {
         this.setState({ currentTarget: target || this.getCurrentSectionTarget() });
     },
+    /**
+     * Scroll to an element with animation
+     * @param {jQuery|HTMLElement} element
+     * @param {object} options - scrollTo options
+     */
     scrollTo: function scrollTo(element, options) {
         options.onAfter = (function () {
             this.shouldReactOnScroll = true;
         }).bind(this);
         $scrollTo(element, options);
     },
+    /**
+     * Fired when a bar item was clicked
+     * @param {string} target
+     */
     onItemClick: function onItemClick(target) {
         this.shouldReactOnScroll = false;
         this.scrollTo(this.getSectionElement(target), this.getOptions().scrollTo);
         this.updatesLocation(target);
         this.setState({ currentTarget: target });
     },
+    /**
+     * Try to retrieve the current section target (match "visible" area)
+     * @returns {string}
+     */
     getCurrentSectionTarget: function getCurrentSectionTarget() {
         var returnValue = null;
         var sections = [];
@@ -259,16 +332,31 @@ var SimpleInpagenav = React.createClass({
         }
         return returnValue;
     },
+    /**
+     * Updates window location hash
+     * @param {string} target
+     */
     updatesLocation: function updatesLocation(target) {
         window.location.hash = '#' + target;
     },
+    /**
+     * Check if we can use the location hash to scroll to a section
+     * @returns {boolean}
+     */
     hasUsableLocation: function hasUsableLocation() {
         var target = this.getTargetFromLocation();
         return this.sections[target] ? true : false;
     },
+    /**
+     * Get the target string from the current window location
+     * @returns {string|null}
+     */
     getTargetFromLocation: function getTargetFromLocation() {
         return window.location.hash ? window.location.hash.substr(1) : null;
     },
+    /**
+     * Listen our custom "debounced" scroll event
+     */
     debouncedScroll: function debouncedScroll() {
         var scrollTimer;
         this.$window.on(CONSTANTS.SCROLL_EVENT, (function (e) {
@@ -280,31 +368,63 @@ var SimpleInpagenav = React.createClass({
             }).bind(this), CONSTANTS.DEBOUNCED_SCROLL_EVENT_DELAY);
         }).bind(this));
     },
+    /**
+     * Check if scroll reached the bottom of the page
+     * @returns {boolean}
+     */
     scrollReachTheBottom: function scrollReachTheBottom() {
         return this.$window.scrollTop() + this.$window.height() === $(document).height();
     },
+    /**
+     * Check if pos could be interpreted as a visible area
+     * @param {number} pos
+     * @returns {boolean}
+     */
     isTheVisibleArea: function isTheVisibleArea(pos) {
         var windowPos = this.$window.scrollTop();
         var windowHeight = Math.round(this.$window.height() * this.getOptions().scrollThreshold);
 
         return pos - windowHeight < windowPos + this.getOptions().scrollOffset;
     },
+    /**
+     * Return a section element
+     * @param {string} target
+     * @returns {jQuery|HTMLElement}
+     */
     getSectionElement: function getSectionElement(target) {
         return $('#' + this.getSectionId(target));
     },
+    /**
+     * Return the DOM id for a section
+     * @param {string} target
+     * @returns {string}
+     */
     getSectionId: function getSectionId(target) {
         return target + CONSTANTS.SECTION_ID_SUFFIX;
     },
+    /**
+     * Get section position
+     * @param {string} id - section DOM id
+     * @returns {number}
+     */
     getSectionPos: function getSectionPos(id) {
         var $section = $('#' + id);
         return Math.round($section.offset().top);
     },
+    /**
+     * Add a section
+     * @param {string} target
+     * @param {string} id - section DOM id
+     */
     registerSection: function registerSection(target, id) {
         if (this.sections[target]) {
             throw 'Target for a section must be unique!';
         }
         this.sections[target] = this.getSectionPos(id);
     },
+    /**
+     * Updates section positions (example: when user resize the window)
+     */
     updatesSectionPositions: function updatesSectionPositions() {
         for (var target in this.sections) {
             if (this.sections.hasOwnProperty(target)) {
@@ -312,6 +432,10 @@ var SimpleInpagenav = React.createClass({
             }
         }
     },
+    /**
+     * Get component options
+     * @returns {object}
+     */
     getOptions: function getOptions() {
         return this.options;
     }
@@ -324,5 +448,5 @@ SimpleInpagenav.BarItem = BarItem;
 module.exports = SimpleInpagenav;
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}]},{},[1])(1)
+},{"./Bar":1,"./BarItem":2,"./Constants":3,"./Section":4}]},{},[5])(5)
 });
