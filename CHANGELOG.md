d6c8365c9869c8e926f74c9be83c5291bafc7b05
Ruben Barilani <ruben.barilani@zalando.de>
2015-12-16 15:36:05 +0100

    (chore): change changelog format, use conventional changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,4 @@
-CHANGELOG
-=========
+<a name="0.1.0"></a>
+# [0.1.0](https://github.com/zalando/react-simple-inpagenav/compare/0.1.0...v0.1.0) (2015-12-16)
 
-# 0.1.0 (2015-12-06)
-
-First "tagged" version
+* First release
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
 var initGulpTasks = require('react-component-gulp-tasks');
+var conventionalChangelog = require('gulp-conventional-changelog');
 
 /**
  * Tasks are added by the react-component-gulp-tasks package
- * 
+ *
  * See https://github.com/JedWatson/react-component-gulp-tasks
  * for documentation.
- * 
+ *
  * You can also add your own additional gulp tasks if you like.
  */
 
@@ -41,3 +42,14 @@ var taskConfig = {
 };
 
 initGulpTasks(gulp, taskConfig);
+
+
+gulp.task('changelog', function () {
+    return gulp.src('CHANGELOG.md', {
+        buffer: false
+    })
+    .pipe(conventionalChangelog({
+        preset: 'angular'
+    }))
+    .pipe(gulp.dest('./'));
+});
